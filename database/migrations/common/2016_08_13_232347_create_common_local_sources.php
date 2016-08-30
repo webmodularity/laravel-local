6b52f86c34c658582ee71210496461d8993f2887
Rory Hellyer <rory@webmodularity.com>
2016-08-30 14:14:24 -0700

    dev

--- database/migrations/common/2016_08_13_232347_create_common_local_sources.php
+++ database/migrations/common/2016_08_13_232347_create_common_local_sources.php
@@ -12,16 +12,14 @@ class CreateCommonLocalSources extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable('common.local_sources')) {
-            Schema::create('common.local_sources', function (Blueprint $table) {
-                $table->smallIncrements('id');
-                $table->string('name', 50);
-                $table->string('url', 255);
-                $table->string('url_review', 255)->nullable();
-                $table->string('url_review_user', 255)->nullable();
-                $table->unique(['name']);
-            });
-        }
+        Schema::create('common.local_sources', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name', 50);
+            $table->string('url', 255);
+            $table->string('url_review', 255)->nullable();
+            $table->string('url_review_user', 255)->nullable();
+            $table->unique(['name']);
+        });
     }
 
     /**
